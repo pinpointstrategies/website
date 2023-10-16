73daa3a12bf793c46654df8deaabde0ba2300313
Jonathan Sidi <yonis@metrumrg.com>
2023-10-16 09:01:43 -0400

    update submit to post to lambda

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,6 +1,6 @@
 /**
  * POST /api/submit
- */
+*/
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
@@ -16,14 +16,33 @@ export async function onRequestPost(context) {
         output[key] = [].concat(tmp, value);
       }
     }
-
-    let pretty = JSON.stringify(output, null, 2);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
   } catch (err) {
     return new Response('Error parsing JSON content', { status: 400 });
   }
+
+	// Use your API endpoint URL you copied from the previous step
+  const endpoint =
+    "<https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/default/sendContactEmail>";
+
+	const body = {body: output};
+
+  const requestOptions = {
+    method: "POST",
+    body
+  };
+
+  fetch(endpoint, requestOptions)
+    .then((response) => {
+      if (!response.ok) throw new Error("Error in fetch");
+      return response.json();
+    })
+    .then((response) => {
+      document.getElementById("result-text").innerText =
+        "Email sent successfully!";
+    })
+    .catch((error) => {
+      document.getElementById("result-text").innerText =
+        "An unkown error occured.";
+    });
+
 }
