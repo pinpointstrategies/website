08b8de0e3030e70c1c42d36dbecc0327c5e196d9
Jonathan Sidi <yonis@metrumrg.com>
2023-10-16 10:02:43 -0400

    revert submit script

--- docs/functions/api/submit.js
+++ docs/functions/api/submit.js
@@ -1,6 +1,6 @@
 /**
  * POST /api/submit
- */
+*/
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
@@ -18,6 +18,8 @@ export async function onRequestPost(context) {
     }
 
     let pretty = JSON.stringify(output, null, 2);
+    console.print(pretty)
+
     return new Response(pretty, {
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
@@ -26,4 +28,43 @@ export async function onRequestPost(context) {
   } catch (err) {
     return new Response('Error parsing JSON content', { status: 400 });
   }
+
 }
+
+/**
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  // prevent the form submit from refreshing the page
+  event.preventDefault();
+
+  const { name, email, message } = event.target;
+
+	// Use your API endpoint URL you copied from the previous step
+  const endpoint =
+    "<https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/default/sendContactEmail>";
+  // We use JSON.stringify here so the data can be sent as a string via HTTP
+	const body = JSON.stringify({
+    senderName: name.value,
+    senderEmail: email.value,
+    message: message.value
+  });
+  const requestOptions = {
+    method: "POST",
+    body
+  };
+
+  fetch(endpoint, requestOptions)
+    .then((response) => {
+      if (!response.ok) throw new Error("Error in fetch");
+      return response.json();
+    })
+    .then((response) => {
+      document.getElementById("result-text").innerText =
+        "Email sent successfully!";
+    })
+    .catch((error) => {
+      document.getElementById("result-text").innerText =
+        "An unkown error occured.";
+    });
+});
+*/
--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,20 +1,51 @@
+/**
+ * POST /api/submit
+*/
+export async function onRequestPost(context) {
+  try {
+    let input = await context.request.formData();
+
+    // Convert FormData to JSON
+    // NOTE: Allows multiple values per key
+    let output = {};
+    for (let [key, value] of input) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
+    }
+
+    let pretty = JSON.stringify(output, null, 2);
+    console.print(pretty)
+
+    return new Response(pretty, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
+  }
+
+}
+
+/**
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   // prevent the form submit from refreshing the page
   event.preventDefault();
 
-  const { name, jobtitle, companyname, email, helptype, message } = event.target;
+  const { name, email, message } = event.target;
 
 	// Use your API endpoint URL you copied from the previous step
   const endpoint =
     "<https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/default/sendContactEmail>";
   // We use JSON.stringify here so the data can be sent as a string via HTTP
 	const body = JSON.stringify({
-    name: name.value,
-    jobtitle: jobtitle.value,
-    companyname: companyname.value,
-    email: email.value,
-    helptype: helptype.value,
+    senderName: name.value,
+    senderEmail: email.value,
     message: message.value
   });
   const requestOptions = {
@@ -36,3 +67,4 @@ form.addEventListener("submit", (event) => {
         "An unkown error occured.";
     });
 });
+*/
