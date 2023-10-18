f671568036381af34fab85e54f222a93d086bd3c
Jonathan Sidi <yonis@metrumrg.com>
2023-10-18 07:24:48 -0400

    fix submit js script

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -3,20 +3,22 @@ if (form!==null){
   form.addEventListener("submit", (event) => {
     // prevent the form submit from refreshing the page
     event.preventDefault();
-    const { name, email, message } = event.target;
+    const { name, job, company, email, category, message } = event.target;
 
   	// Use your API endpoint URL you copied from the previous step
-    const endpoint =
-      "https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/addMethods/sendContactEmail"
+    const endpoint = "https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/default/sendContactEmail"
+
     // We use JSON.stringify here so the data can be sent as a string via HTTP
+
   	const body = JSON.stringify({
       name: name.value,
-      jobtitle: jobtitle.value,
-      companyname: companyname.value,
+      job: job.value,
+      company: company.value,
       email: email.value,
-      helptype: helptype.value,
+      category: category.value,
       message: message.value
     });
+
     const requestOptions = {
       method: "POST",
       body
@@ -24,7 +26,7 @@ if (form!==null){
 
     fetch(endpoint, requestOptions)
       .then((response) => {
-        console.log(response.json());
+        console.log("response.statusText =", response.statusText);
         if (!response.ok) throw new Error("Error in fetch");
         return response.json();
       })
