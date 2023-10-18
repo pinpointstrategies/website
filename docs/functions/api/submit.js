6b9931bf1115707a2b1acc2219b9505157fdb348
Jonathan Sidi <yonis@metrumrg.com>
2023-10-17 23:28:25 -0400

    move submit back under functions/api

---
+++ docs/functions/api/submit.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("form");
+if (form!==null){
+  form.addEventListener("submit", (event) => {
+    // prevent the form submit from refreshing the page
+    event.preventDefault();
+    const { name, email, message } = event.target;
+
+  	// Use your API endpoint URL you copied from the previous step
+    const endpoint =
+      "https://58ho8blr2h.execute-api.us-east-2.amazonaws.com/addMethods/sendContactEmail"
+    // We use JSON.stringify here so the data can be sent as a string via HTTP
+  	const body = JSON.stringify({
+      name: name.value,
+      jobtitle: jobtitle.value,
+      companyname: companyname.value,
+      email: email.value,
+      helptype: helptype.value,
+      message: message.value
+    });
+    const requestOptions = {
+      method: "POST",
+      body
+    };
+
+    fetch(endpoint, requestOptions)
+      .then((response) => {
+        console.log(response.json());
+        if (!response.ok) throw new Error("Error in fetch");
+        return response.json();
+      })
+      .then((response) => {
+        document.getElementById("result-text").innerText =
+          "Email sent successfully!";
+      })
+      .catch((error) => {
+        console.log(error);
+        document.getElementById("result-text").innerText =
+          "An unkown error occured.";
+      });
+  });
+
+}
